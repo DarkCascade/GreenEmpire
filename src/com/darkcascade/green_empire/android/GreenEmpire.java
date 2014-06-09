66cf346a0ee5aaba056d4a4325c2b823bfd20673
DarkCascade <DarkCascade@gmail.com>
2014-06-08 19:24:55 -0600

    i wonder if we can make this one project

---
+++ src/com/darkcascade/green_empire/android/GreenEmpire.java
@@ -0,0 +1,30 @@
+package com.darkcascade.green_empire.android;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GreenEmpire extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	BitmapFont text;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		text.draw(batch, "Harvest", 20, Gdx.graphics.getHeight() - 40);
+		batch.end();
+	}
+}
